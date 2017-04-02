e1becd9351f6c494895a55f34c671c029a2facf3
Alocias <alocias0@gmail.com>
2017-04-02 14:10:56 +0200

    trying to reduce by hand

--- project2/scripts/diff2.py
+++ project2/scripts/diff2.py
@@ -10,7 +10,7 @@ r2sq = r2**2
 dx12 = x1-x2
 dy12 = y1-y2
 r12 = sp.sqrt(dx12**2 + dy12**2)
-r12sq = r12**2
+r12sq = dx12**2 + dy12**2
 
 psiT = sp.simplify(sp.exp(a*w/2*(r1sq+r2sq))*sp.exp(d*r12/(1+b*r12)))
 
@@ -22,12 +22,12 @@ diff2PsiTy2 = sp.simplify(sp.diff(psiT, y2, 2))
 replaceDict = {r1:sp.symbols('r_1'), 
                 r2:sp.symbols('r_2'), 
                 r12:sp.symbols('r_{12}'),
-                dx12:sp.symbols('x_{12}'),
-                dy12:sp.symbols('y_{12}'),
                 r1sq:sp.symbols('r^2_1'),
                 r2sq:sp.symbols('r^2_2'),
-                r12sq:sp.symbols('r^2_{12}')}
+                r12sq:sp.symbols('r^2_{12}'),
+                dx12:sp.symbols('x_{12}'),
+                dy12:sp.symbols('y_{12}')}
 
-print sp.latex(sp.simplify(sp.simplify(0.5*(sp.simplify(-(diff2PsiTx1 +
+print sp.latex(sp.simplify(sp.factor(sp.simplify(sp.simplify(0.5*(sp.simplify(-(diff2PsiTx1 +
     diff2PsiTy1 + diff2PsiTx2 + diff2PsiTy2))/psiT + w**2*(r1sq + r2sq)) +
-    1/r12).subs(replaceDict)))
+    1/r12).subs(replaceDict)))))
---
+++ project2/scripts/diff2Polar.py
@@ -0,0 +1,6 @@
+import sympy as sp
+import numpy as np
+
+a, b, w, d, r1, r2 = sp.symbols('\\alpha \\beta \\omega a r1 r2')
+
+r12 = np.sqrt(r1-r2)
