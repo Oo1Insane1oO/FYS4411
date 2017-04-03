713d35c1045c1de3d8722f89b21e590078cdef5a
Alocias <alocias0@gmail.com>
2017-04-03 14:23:57 +0200

    Derivate does not exist

--- project2/scripts/diff2.py
+++ project2/scripts/diff2.py
@@ -20,10 +20,10 @@ replaceDict = {
 psiT = sy.simplify(sy.exp(a*w/2*(r1sq+r2sq)) *
         sy.exp(d*sy.sqrt(r12sq)/(1+b*sy.sqrt(r12sq))))
 
-diff2PsiTx1 = sy.simplify(sy.Derivate(psiT, x1, 2))
-diff2PsiTy1 = sy.simplify(sy.Derivate(psiT, y1, 2))
-diff2PsiTx2 = sy.simplify(sy.Derivate(psiT, x2, 2))
-diff2PsiTy2 = sy.simplify(sy.Derivate(psiT, y2, 2))
+diff2PsiTx1 = sy.simplify(sy.diff(psiT, x1, 2))
+diff2PsiTy1 = sy.simplify(sy.diff(psiT, y1, 2))
+diff2PsiTx2 = sy.simplify(sy.diff(psiT, x2, 2))
+diff2PsiTy2 = sy.simplify(sy.diff(psiT, y2, 2))
 
 print
 sy.latex(sy.simplify(sy.simplify(0.5*(sy.simplify(-(diff2PsiTx1
