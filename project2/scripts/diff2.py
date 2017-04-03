044f2174b839e63b6beb74ba30657fdd7828b8f6
Alocias <alocias0@gmail.com>
2017-04-03 13:48:58 +0200

    fixed squareroot problem, need to check

--- project2/scripts/diff2.py
+++ project2/scripts/diff2.py
@@ -1,34 +1,31 @@
-import sympy as sp
+import sympy as sy
 import numpy as np
 
-a, b, w, d, x1, x2, y1, y2 = sp.symbols('\\alpha \\beta \\omega a x1 x2 y1 y2')
+x1, x2, y1, y2 = sy.symbols('x_1 x_2 y_1 y_2', real=True)
+a, b, w, d = sy.symbols('\\alpha \\beta \\omega a', real=True, positive=True)
 
-r1 = sp.sqrt(x1*x1 + y1*y1)
-r2 = sp.sqrt(x2*x2 + y2*y2)
-r1sq = r1**2
-r2sq = r2**2
+r1sq = x1*x1 + y1*y1
+r2sq = x2*x2 + y2*y2
 dx12 = x1-x2
 dy12 = y1-y2
-r12 = sp.sqrt(dx12**2 + dy12**2)
 r12sq = dx12**2 + dy12**2
 
-replaceDict = {r1:sp.symbols('r_1'), 
-                r2:sp.symbols('r_2'), 
-                r12:sp.symbols('r_{12}'),
-                r1sq:sp.symbols('r^2_1'),
-                r2sq:sp.symbols('r^2_2'),
-                r12sq:sp.symbols('r^2_{12}'),
-                dx12:sp.symbols('x_{12}'),
-                dy12:sp.symbols('y_{12}')}
+replaceDict = {
+        r1sq:sy.symbols('r^2_1')**2,
+        r2sq:sy.symbols('r^2_2')**2,
+        dx12:sy.symbols('x_{12}'),
+        dy12:sy.symbols('y_{12}'),
+        r12sq:sy.symbols('r^2_{12}')**2}
 
-psiT = sp.simplify(sp.exp(a*w/2*(r1sq+r2sq))*sp.exp(d*r12/(1+b*r12)))
+psiT = sy.simplify(sy.exp(a*w/2*(r1sq+r2sq)) *
+        sy.exp(d*sy.sqrt(r12sq)/(1+b*sy.sqrt(r12sq))))
 
-diff2PsiTx1 = sp.simplify(sp.simplify(sp.diff(psiT, x1, 2)).subs(replaceDict))
-diff2PsiTy1 = sp.simplify(sp.simplify(sp.diff(psiT, y1, 2)).subs(replaceDict))
-diff2PsiTx2 = sp.simplify(sp.simplify(sp.diff(psiT, x2, 2)).subs(replaceDict))
-diff2PsiTy2 = sp.simplify(sp.simplify(sp.diff(psiT, y2, 2)).subs(replaceDict))
+diff2PsiTx1 = sy.simplify(sy.Derivate(psiT, x1, 2))
+diff2PsiTy1 = sy.simplify(sy.Derivate(psiT, y1, 2))
+diff2PsiTx2 = sy.simplify(sy.Derivate(psiT, x2, 2))
+diff2PsiTy2 = sy.simplify(sy.Derivate(psiT, y2, 2))
 
 print
-sp.latex(sp.simplify(sp.simplify(sp.collect(sp.simplify(sp.collect(sp.simplify(0.5*(sp.simplify(-(diff2PsiTx1
+sy.latex(sy.simplify(sy.simplify(0.5*(sy.simplify(-(diff2PsiTx1
     + diff2PsiTy1 + diff2PsiTx2 + diff2PsiTy2))/psiT + w**2*(r1sq + r2sq)) +
-    1/r12), 2*b**3*a/(b*r12+1)**4)), b*a/(b*r12+1)**2)).subs(replaceDict)))
+    1/sy.sqrt(r12sq)).subs(replaceDict)))
