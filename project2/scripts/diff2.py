719b2252c75c548138ebe19e3689aa433eb5a8c4
Alocias <alocias0@gmail.com>
2017-04-05 20:34:16 +0200

    stupid

--- project2/scripts/diff2.py
+++ project2/scripts/diff2.py
@@ -17,11 +17,11 @@ def collectDenom(expr):
             commons[sy.denom(arg)] += sy.numer(arg)
         else:
             commons[sy.denom(arg)] = sy.numer(arg)
-    return sy.Add(*[sy.simplify(commons[denom])/denom for denom in commons])
+    return sum([sy.simplify(commons[denom])/denom for denom in commons])
 # end function collectDenom
 
-x1, x2, y1, y2 = sy.symbols('x_1 x_2 y_1 y_2', real=True)
-a, b, w, d = sy.symbols('\\alpha \\beta \\omega a', real=True, positive=True)
+a, b, x1, x2, y1, y2 = sy.symbols('\\alpha \\beta x_1 x_2 y_1 y_2', real=True)
+w, d = sy.symbols('\\omega a', real=True, positive=True)
 
 r1sq = x1**2 + y1**2
 r2sq = x2**2 + y2**2
@@ -34,6 +34,11 @@ replaceDict = makeDict({r1sq:['r_1',2],r2sq:['r_2',2],r12sq:['r_{12}',2]})
 psiT = sy.simplify(sy.exp(a*w/2*(r1sq+r2sq)) *
         sy.exp(d*sy.sqrt(r12sq)/(1+b*sy.sqrt(r12sq))))
 
+# r12 = sy.sqrt(r12sq)
+# expr = 1 / (r12**2 * (b*r12+1)**4*(b*(x1-x2)**2+b*(y1-y2)**2+r12))
+
+# print collectDenom(1/x1 + y1/x1 + y2/y1 + 0.5)
+ 
 diff2PsiTx1 = sy.simplify(sy.simplify(sy.diff(psiT, x1, 2)).subs(replaceDict))
 diff2PsiTy1 = sy.simplify(sy.simplify(sy.diff(psiT, y1, 2)).subs(replaceDict))
 diff2PsiTx2 = sy.simplify(sy.simplify(sy.diff(psiT, x2, 2)).subs(replaceDict))
@@ -42,4 +47,4 @@ diff2PsiTy2 = sy.simplify(sy.simplify(sy.diff(psiT, y2, 2)).subs(replaceDict))
 total = collectDenom(sy.simplify(0.5*(sy.simplify(-(diff2PsiTx1 + diff2PsiTy1 +
     diff2PsiTx2 + diff2PsiTy2)/psiT) + w**2*(r1sq + r2sq)) + 1/sy.sqrt(r12sq)))
 
-print sy.latex(sy.simplify(total.subs(replaceDict)))
+print sy.latex(sy.simplify(sy.collect(sy.simplify(total.subs(replaceDict)), (b, b**2, b**3)).subs(replaceDict)))
