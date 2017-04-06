96841e2bb69ebd690539035fb3f8da2757f9447f
Alocias <alocias0@gmail.com>
2017-04-06 22:55:28 +0200

    reduced

--- project2/scripts/diff2.py
+++ project2/scripts/diff2.py
@@ -9,7 +9,7 @@ def makeDict(inputDict):
     return returnDict
 # end function makeDict
 
-def collectDenom(expr):
+def collectDenom(expr, subsDict):
     """ collect input expression in terms of common denominators """
     commons = {}
     for arg in expr.args:
@@ -17,7 +17,7 @@ def collectDenom(expr):
             commons[sy.denom(arg)] += sy.numer(arg)
         else:
             commons[sy.denom(arg)] = sy.numer(arg)
-    return sum([sy.simplify(commons[denom])/denom for denom in commons])
+    return sum([sy.simplify(commons[denom]).subs(subsDict)/denom for denom in commons])
 # end function collectDenom
 
 a, b, x1, x2, y1, y2 = sy.symbols('\\alpha \\beta x_1 x_2 y_1 y_2', real=True)
@@ -31,36 +31,21 @@ r12sq = dx12**2 + dy12**2
 sym05 = sy.Rational(0.5)
 
 replaceDict = makeDict({r1sq:['r_1',2],r2sq:['r_2',2],r12sq:['r_{12}',2]})
+collectList = (b, b**2, b**3, replaceDict[r12sq], d, a**2*w**2)
 
 psiT = sy.simplify(sy.exp(a*w/2*(r1sq+r2sq)) *
         sy.exp(d*sy.sqrt(r12sq)/(1+b*sy.sqrt(r12sq))))
 
-# r12 = sy.sqrt(r12sq)
-# expr = 1 / (r12**2 * (b*r12+1)**4*(b*(x1-x2)**2+b*(y1-y2)**2+r12))
-
-# print collectDenom(1/x1 + y1/x1 + y2/y1 + 0.5)
- 
-diff2PsiTx1 = sy.simplify(sy.diff(psiT, x1, 2))
-diff2PsiTy1 = sy.simplify(sy.diff(psiT, y1, 2))
-diff2PsiTx2 = sy.simplify(sy.diff(psiT, x2, 2))
-diff2PsiTy2 = sy.simplify(sy.diff(psiT, y2, 2))
+diff2PsiTx1 = sy.simplify(sy.simplify(sy.diff(psiT, x1, 2)).subs(replaceDict))
+diff2PsiTy1 = sy.simplify(sy.simplify(sy.diff(psiT, y1, 2)).subs(replaceDict))
+diff2PsiTx2 = sy.simplify(sy.simplify(sy.diff(psiT, x2, 2)).subs(replaceDict))
+diff2PsiTy2 = sy.simplify(sy.simplify(sy.diff(psiT, y2, 2)).subs(replaceDict))
 
 sumTotal = diff2PsiTx1 + diff2PsiTy1 + diff2PsiTx2 + diff2PsiTy2
-collectList = (b, b**2, b**3, replaceDict[r12sq], a)
-total = sy.simplify(
-            sy.collect(
-                sy.simplify(
-                    sy.collect(
-                        sy.simplify(
-                            sy.nsimplify(
-                                collectDenom(
-                                    sy.simplify(-sym05*sumTotal/psiT 
-                                                + sym05*w**2*(r1sq + r2sq) 
-                                                + 1/sy.sqrt(r12sq)))
-                                    .subs(replaceDict), rational=True)), 
-                            collectList).subs(replaceDict)), collectList)
-            .subs(collectList))
+total = collectDenom(sy.nsimplify(sym05 * (-sumTotal/psiT + w**2*(r1sq + r2sq))
+    + 1/sy.sqrt(r12sq), rational=True), replaceDict)
+total = collectDenom(sy.simplify(sy.collect(sy.collect(total,
+    collectList).subs(replaceDict), collectList)).subs(replaceDict),
+    replaceDict)
 
 print sy.latex(total)
-
-# print sy.latex(sy.collect(sy.simplify(sy.nsimplify(sy.simplify(sy.collect(sy.simplify(total.subs(replaceDict)), (b, b**2, b**3)).subs(replaceDict)).subs(replaceDict), rational=True)).subs(replaceDict), 2*d*(b*sy.sqrt(r12sq+1))**3).subs(replaceDict))
