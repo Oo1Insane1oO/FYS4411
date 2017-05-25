9dc25429641f3365071be339b2fadad4fa4570dd
Alocias <alocias0@gmail.com>
2017-05-25 16:51:44 +0200

    need to fix jastrow

--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -53,6 +53,7 @@ int main(int argc, const char** argv) {
     std::cout << "Basis made" << std::endl;
     
     // set vmc object for calculations
+//     VMC *vmcObj = new VMC(b,1,0.4,2,step,maxIterations,imp);
     VMC *vmcObj = new VMC(b,1,0.4,2,step,maxIterations,imp);
 //     VMC *vmcObj = new VMC(b,1.03741,0.472513,2,step,maxIterations,imp);
     
