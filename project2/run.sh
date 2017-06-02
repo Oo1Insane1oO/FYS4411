9c4eea1992b2e4403a0d5e5233e02d4c3e484128
Alocias <alocias0@gmail.com>
2017-06-02 15:43:27 +0200

    running for 1 0.1 and 0.5

--- project2/run.sh
+++ project2/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for E in 2 6 12 20
 do
-    for OMEGA in 1 2 3 4 5
+    for OMEGA in 10 1 5
     do
         mpirun -np 8 ./vmc_main `bc<<<"scale=2;"$OMEGA"/10"` $E 1000000 0.00035 0 1 1 1 "data/w0"$OMEGA"/N"$E"/w"$OMEGA"_N"$E"_"
     done
