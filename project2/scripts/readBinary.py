fbecfe4f0499bfe5656d2c9a15f4d21f6b836241
Alocias <alocias0@gmail.com>
2017-06-04 00:16:23 +0200

    made script for reading data

--- project2/scripts/readBinary.py
+++ project2/scripts/readBinary.py
@@ -1,10 +1,32 @@
 import numpy as np
 import sys
+import os
 
-filename = sys.argv[1];
-data = np.fromfile(filename, dtype=np.float64, count=-1, sep="");
+alpha = 0
+beta = 0
+total = 0
+potential = 0
+kinetic = 0
+count = 0
+j = 0
+directory = sys.argv[1];
+for filename in os.listdir(directory):
+    data = np.fromfile(directory+filename, dtype=np.float64, count=-1, sep="");
 
-newData = np.zeros((len(data),6))
+    alpha += data[-2]
+    beta += data[-1]
+    i = 0
+    while i<len(data[:-2]):
+        total += data[i]
+        potential += data[i+1]
+        kinetic += data[i+2]
+        i += 3
+        j += 1
 
-for i in range(len(data)/6):
-    newData[i] = data[i:i+6]
+    count += 1
+
+print total/j
+print potential/j
+print kinetic/j
+print alpha/count
+print beta/count
